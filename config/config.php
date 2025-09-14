425a9a714e0d73f051d9eb71bfebd1520b3f5b89
dbfacha0529 <satoshi3104210@gmail.com>
2025-09-14 22:31:08 +0900

    first commit

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+error_reporting(E_ALL & ~E_WARNING);
+ini_set('date.timezone','Asia/Tokyo');
+
+define('DB_HOST','localhost');
+define('DB_NAME','reserve');
+define('DB_USER','root');
+define('DB_PASSWORD','');
+
---
+++ web/index.php
@@ -0,0 +1,33 @@
+
+
+<!doctype html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ログイン画面</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+        <!--オリジナルCSS-->
+    <link href = "./css/login.css" rel="stylesheet">
+  </head>
+  <body>
+
+    <img src="../img/Hennessy.jpg">
+    <div class="mb-3">
+  <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="User_ID">
+</div>
+
+    <div class="mb-3">
+  <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="Password">
+</div>
+
+<button type="button" class="btn btn-Login">Log in</button>
+
+<a type="button" class="btn btn-Create" href="sign_up.php">新規登録</a>
+
+<a type="button" class="btn btn-Forgot" href="forgot.php">お忘れの方</a>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+  </body>
+</html>
