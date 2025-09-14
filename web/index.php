395eb0ac5b15eff056629d5403c17e05014c7608
dbfacha0529 <satoshi3104210@gmail.com>
2025-09-14 22:50:24 +0900

    テスト #1

--- web/index.php
+++ web/index.php
@@ -5,7 +5,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>ログイン画面だよ</title>
+    <title>ログイン画面</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
         <!--オリジナルCSS-->
     <link href = "./css/login.css" rel="stylesheet">
